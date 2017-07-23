a283903937fa75f9f63c42509190ab8f1c809050
lokideos <archmage09@gmail.com>
2017-07-23 16:23:02 +0300

    Add routes and tests for index answers action in its controller

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe AnswersController, type: :controller do
+  let(:answer) { create(:answer) }
+  let(:question) { create(:question) }
+
+  describe 'GET #index' do    
+    let(:answers) { question; create_list(:answer, 2) }
+
+    before { get :index, params: { question_id: question.id } }
+
+    it 'populates array array of all answers to this questions' do      
+      expect(assigns(:answers)).to match_array(answers)
+    end
+
+    it 'render index view' do      
+      expect(response).to render_template :index
+    end
+  end
 
 end
