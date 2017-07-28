eb1224b05a8e5e9ed5af7344aafd3e789d999c3a
lokideos <archmage09@gmail.com>
2017-07-29 00:03:32 +0300

    Check questions list; status: GREEN

--- spec/acceptance/inspect_questions_list.rb
+++ spec/acceptance/inspect_questions_list.rb
@@ -7,23 +7,31 @@ feature 'Inspect question list', %q{
 } do 
 
   given(:user) { create(:user) }
+  given(:question) { create(:question) }
 
   scenario 'Authenticated user check questions list' do
+    
     sign_in(user)
-    Question.create!(title: "Test Question", body: "Test text")
+
+    visit questions_path
+    click_on 'Ask question'
+    fill_in 'Title', with: question.title
+    fill_in 'Body', with: question.body
+    click_on 'Create'
 
     visit questions_path
 
     expect(page).to have_content "Questions List"
-    expect(page).to have_content "Test Question"
+    expect(page).to have_content question.title
   end
 
   scenario 'Non-authenticated user check questions list' do
+    Question.create!(title: question.title, body: question.body)
+
     visit questions_path
 
-    Question.create!(title: "Test Question", body: "Test text")
-    expect(page).to have_content "Questions List"    
-    expect(page).to have_content "Test Question"
+    expect(page).to have_content "Questions List"
+    expect(page).to have_content question.title
   end
   
 end
