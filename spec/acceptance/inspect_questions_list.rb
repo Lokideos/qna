5325be4e4b868ba4b4ac2f3bca03354cc8f71323
lokideos <archmage09@gmail.com>
2017-07-28 23:24:04 +0300

    Check questions list, status: RED

---
+++ spec/acceptance/inspect_questions_list.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+feature 'Inspect question list', %q{
+  In order to answer to specific question
+  As an User
+  I want to be able to check questions' list
+} do 
+
+  given(:user) { create(:user) }
+
+  scenario 'Authenticated user check questions list' do
+    sign_in(user)
+    Question.create!(title: "Test Question", body: "Test text")
+
+    visit questions_path
+
+    expect(page).to have_content "Questions List"
+    expect(page).to have_content "Test Question"
+  end
+
+  scenario 'Non-authenticated user check questions list' do
+    visit questions_path
+
+    expect(page).to have_content "Questions List"
+    expect(page).to have_content "Test Question"
+  end
+  
+end
+
