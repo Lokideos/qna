a9c549ecdf103ee33b4303f2dc98c60fd37a9832
lokideos <archmage09@gmail.com>
2017-08-01 15:41:49 +0300

    Add invalid parameteres tests and improve creation tests

--- spec/acceptance/create_answer_to_question_spec.rb
+++ spec/acceptance/create_answer_to_question_spec.rb
@@ -8,7 +8,7 @@ feature 'Create answer to question', %q{
 
   given(:user) { create(:user) }
 
-  scenario 'Authenticated user creates answer' do
+  scenario 'Authenticated user creates answer with valid parameters' do
     sign_in(user)
     create_question
 
@@ -19,6 +19,20 @@ feature 'Create answer to question', %q{
     click_on 'Create Answer'
 
     expect(page).to have_content 'Test Answer'
+    expect(page).to have_content 'Answer was created.'    
+  end
+
+  scenario 'Authenticated user creates answer with invalid parameters' do
+    sign_in(user)
+    create_question
+
+    visit questions_path
+    click_on 'Show'
+
+    fill_in "Add New Answer", with: nil
+    click_on 'Create Answer'
+
+    expect(page).to have_content "Data for your answer contained 1 error: Body can't be blank"
   end
 
   scenario 'Non-authenticated user tries to create answer' do
--- spec/acceptance/create_question_spec.rb
+++ spec/acceptance/create_question_spec.rb
@@ -8,7 +8,7 @@ feature 'Create question', %q{
 
   given(:user) { create(:user) }
 
-  scenario 'Authenticated user creates question' do
+  scenario 'Authenticated user creates question with valid parameters' do
     sign_in(user)
 
     visit questions_path
@@ -18,6 +18,20 @@ feature 'Create question', %q{
     click_on 'Create'
 
     expect(page).to have_content 'Your question successfully created.'
+    expect(page).to have_content 'Test question'
+    expect(page).to have_content 'text text text'
+  end
+
+  scenario 'Authenticated user creates question with invalid parameters' do
+    sign_in(user)
+
+    visit questions_path
+    click_on 'Ask question'
+    fill_in 'Title', with: nil
+    fill_in 'Body', with: nil
+    click_on 'Create'
+
+    expect(page).to have_content '2 errors were found in the data you typed in.'
   end
 
   scenario 'Non-authenticated user tries to create question' do
