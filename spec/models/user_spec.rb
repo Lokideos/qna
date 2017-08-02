d95bad2cfadf1fa5a721511672db5862d67ed1b5
lokideos <archmage09@gmail.com>
2017-08-02 12:45:06 +0300

    Add author_of? method tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,9 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe User do
-  it { should have_many :answers }
-  it { should have_many :questions }
+  context 'associations' do
+    it { should have_many :answers }
+    it { should have_many :questions }
+  end
 
-  it { should validate_presence_of :email }
-  it { should validate_presence_of :password }
+  context 'methods test' do
+    it "author_of? method should return true" do
+      user = create(:user)
+      question = create(:question, user: user)
+      expect(user.author_of?(question)).to eq true
+    end
+
+  end
+
+  context 'validations' do
+    it { should validate_presence_of :email }
+    it { should validate_presence_of :password }
+  end
 end
