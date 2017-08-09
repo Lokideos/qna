31e2247a4c9533a875c28db38c7fc418d05ef057
lokideos <archmage09@gmail.com>
2017-08-09 21:12:11 +0300

    Fix update form

--- app/assets/javascripts/answers.js
+++ app/assets/javascripts/answers.js
@@ -2,11 +2,11 @@ var ready;
 ready = function() {
   $('.edit-answer-link').click(function(e) {
     e.preventDefault();
+    
     $(this).hide();
     answerId = $(this).data('answerId');
     $('form#edit-answer-' + answerId).show();
   });
 };
 $(document).ready(ready);
-$(document).on('turbolink:load', ready);
-$(document).on('page:update', ready);
+$(document).on('turbolink:load',ready);
