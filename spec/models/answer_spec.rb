c6f6b1dcd0c7f5cc3c8ccf2654d8f5d50016db7f
lokideos <archmage09@gmail.com>
2017-08-10 17:38:39 +0300

    Add choose_best method tests

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -7,6 +7,24 @@ RSpec.describe Answer, type: :model do
     it { should belong_to :user }
   end
 
+  context 'methods test' do
+    let(:user){ create(:user) }
+    let(:question){ create(:question, user: user) }    
+
+    it "choose_best_answer method should update best_answer field" do
+      answer = create(:answer, question: question, user: user)
+      expect(answer.choose_best_answer(answer)).to eq true
+    end
+
+    it "choose_best_answer method should update best_answer field if there is another best answer" do
+      answer = create(:answer, question: question, user: user, best_answer: true)
+      answer2 = create(:answer, question: question, user: user)
+
+      expect(answer.choose_best_answer(answer2)).to eq true
+    end
+
+  end
+
   context "Validations" do
     it { should validate_presence_of :body }
   end
