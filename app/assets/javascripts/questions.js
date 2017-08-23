23eaff5bfd726cb2cd8c17f3434019f3c4e97378
lokideos <archmage09@gmail.com>
2017-08-23 04:51:51 +0300

    Refactoring

--- app/assets/javascripts/questions.js
+++ app/assets/javascripts/questions.js
@@ -5,6 +5,7 @@ ready = function() {
     $('.current_rating').html('');
     $('.current_rating').html('<p>Question Rating: ' + rating + '</p>');
   });
+  
   $('a.rate-bad-question').bind('ajax:success', function (e, data, status, xhr) {
     var rating = $.parseJSON(xhr.responseText);
     $('.current_rating').html('');
