4f84ee3d0cad75901bc48e08679f774b246b025f
lokideos <archmage09@gmail.com>
2017-08-25 11:04:19 +0300

    Remove excessive js handler for answers

--- app/assets/javascripts/answers.js
+++ app/assets/javascripts/answers.js
@@ -8,17 +8,7 @@ ready = function() {
     $('form#edit-answer-' + answerId).show();
   });
 
-  $('a.rate-good-answer').bind('ajax:success', function (e, data, status, xhr) {
-    var rating = $.parseJSON(xhr.responseText);
-    var answerId = $(this).data('answerId');
-    $('.answer-rating-value-' + answerId).html('');
-    $('.answer-rating-value-' + answerId).html('<p>Answer Rating: ' + rating + '</p>');
-    $('.rating-saved-vote-answer-' + answerId).hide();
-    $('.rating-options-vote-answer-' + answerId).hide();
-    $('.rating-options-cancel-answer-' + answerId).show();
-  });
-
-  $('a.rate-bad-answer').bind('ajax:success', function (e, data, status, xhr) {
+  $('a.change-rate-answer').bind('ajax:success', function (e, data, status, xhr) {
     var rating = $.parseJSON(xhr.responseText);
     var answerId = $(this).data('answerId');
     $('.answer-rating-value-' + answerId).html('');
