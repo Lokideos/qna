125c3509a38b9a1342111a7d5d3822b36bd79008
lokideos <archmage09@gmail.com>
2017-08-26 19:46:14 +0300

    Fix tests for ratable concern model

--- spec/models/concerns/ratable_spec.rb
+++ spec/models/concerns/ratable_spec.rb
@@ -12,16 +12,39 @@ describe 'ratable' do
 
   let(:user) { create(:user) }
 
-  it "#add_rating" do
-    expect(WithRatable.create!.add_rating(user).value).to eq 1
+  it "#add_rating if rating exists" do
+    rating = WithRatable.create!.add_rating(user)
+    expect(rating.user).to eq user
+    expect(rating.value).to eq 1
   end
 
-  it "decrease_rating" do
-    expect(WithRatable.create!.decrease_rating(user).value).to eq -1
+  it "#add_rating if rating doesn't exist" do
+    ratable_object = WithRatable.create!
+    ratable_object.decrease_rating(user)
+    ratable_object.add_rating(user)
+    expect(ratable_object.ratings.first.user).to eq user
+    expect(ratable_object.ratings.first.value).to eq 1
   end
 
-  it "nullify_rating" do
-    expect(WithRatable.create!.nullify_rating(user)).to eq nil
+  it "decrease_rating if rating exists" do
+    rating = WithRatable.create!.decrease_rating(user)
+    expect(rating.user).to eq user
+    expect(rating.value).to eq -1
+  end
+
+  it "decrease_rating if rating doesn't existt" do
+    ratable_object = WithRatable.create!
+    ratable_object.add_rating(user)
+    ratable_object.decrease_rating(user)
+    expect(ratable_object.ratings.first.user).to eq user
+    expect(ratable_object.ratings.first.value).to eq -1
+  end
+
+  it "#nullify_rating" do
+    ratable_object = WithRatable.create!
+    ratable_object.add_rating(user)
+    ratable_object.nullify_rating(user)
+    expect(ratable_object.ratings.first).to eq nil
   end
 
   it "should have many ratings" do
