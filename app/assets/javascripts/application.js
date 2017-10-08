48a0ff71113e8074b97c0a7dd69eb76007eacff5
lokideos <archmage09@gmail.com>
2017-10-08 18:12:17 +0300

    ClassWork - Set up app for using WS (add App variable for consumer and set up application js for action cable

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,7 +14,11 @@
 //= require jquery
 //= require jquery.turbolinks
 //= require jquery_ujs
+//= require action_cable
 //= require jquery.remotipart
 //= require cocoon
 //= require turbolinks
 //= require_tree .
+
+var App = App || {};
+App.cable = ActionCable.createConsumer();
