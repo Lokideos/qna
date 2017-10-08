94e5d10591d80441f947e524a59313417cad2515
lokideos <archmage09@gmail.com>
2017-10-09 01:39:46 +0300

    ClassWork - Add tests for broadcasting newly created questions

---
+++ spec/acceptance/see_created_question_spec.rb
@@ -0,0 +1,33 @@
+require 'acceptance_helper'
+
+feature 'see created question', :js do
+  given(:user) { create(:user) }
+
+  context "multiple sessions" do
+    scenario "question appears on another user's page" do
+      Capybara.using_session('user') do
+        sign_in(user)
+        visit questions_path
+      end
+
+      Capybara.using_session('guest') do
+        visit questions_path
+      end
+
+      Capybara.using_session('user') do
+        click_on 'Ask question'
+        fill_in 'Title', with: 'Test question'
+        fill_in 'Body', with: 'text text text'
+        click_on 'Create'
+
+        expect(page).to have_content 'Your question successfully created.'
+        expect(page).to have_content 'Test question'
+        expect(page).to have_content 'text text text'
+      end
+
+      Capybara.using_session('guest') do
+        expect(page).to have_content 'Test question'
+      end
+    end
+  end
+end
