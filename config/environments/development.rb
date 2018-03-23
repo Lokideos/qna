f58a000d8c40117461b5cb64c1242b2108231624
lokideos <archmage09@gmail.com>
2018-03-23 21:56:16 +0300

    Install and set up gems for oauth tests via rspec

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -56,5 +56,10 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
+  # Configuration for letter_opener gem
+  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.perform_deliveries = true
+
+  # Enable https support
   config.force_ssl = true
 end
