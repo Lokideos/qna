f374f516db4619c4488d8db0d28fb3ece8d1ef20
lokideos <archmage09@gmail.com>
2018-03-27 23:51:21 +0300

    Initial oauth twitter setup

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :omniauthable, omniauth_providers: [:facebook]
+         :recoverable, :rememberable, :trackable, :validatable, :omniauthable, omniauth_providers: [:facebook, :twitter]
 
   has_many :answers, dependent: :destroy
   has_many :questions, dependent: :destroy
