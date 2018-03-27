c02f25e24b256acc22d0842218d2d1e3493e2e15
lokideos <archmage09@gmail.com>
2018-03-27 23:19:43 +0300

    Test omniauth controller

---
+++ spec/controllers/omniauth_callbacks_controller_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe OmniauthCallbacksController, type: :controller do
+  before do
+    request.env["devise.mapping"] = Devise.mappings[:user]
+    request.env["omniauth.auth"] = mock_auth_facebook_hash
+  end
+
+  describe 'GET|POST oauth #facebook' do
+    before { get :facebook, params: { omniauth_auth: request.env["omniauth.auth"] } }
+
+    it 'assigns the found user to correct class' do
+      expect(assigns(:user)).to be_a(User)
+    end
+
+    it 'redirects to root path' do
+      expect(response).to redirect_to root_path
+    end
+  end
+end
