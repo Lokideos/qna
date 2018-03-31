5d95676039ab46c7f16e87b3c47cd76dbce05ffe
lokideos <archmage09@gmail.com>
2018-03-31 23:03:10 +0300

    Add acceptance and unit tests for oauth authentication via twitter

--- spec/controllers/omniauth_callbacks_controller_spec.rb
+++ spec/controllers/omniauth_callbacks_controller_spec.rb
@@ -17,4 +17,16 @@ RSpec.describe OmniauthCallbacksController, type: :controller do
       expect(response).to redirect_to root_path
     end
   end
+
+  describe 'GET|POST oauth #twitter' do
+    before { get :twitter, params: { omniauth_auth: request.env["omniauth.auth"] } }    
+
+    it 'assigns the found user to correct class' do
+      expect(assigns(:user)).to be_a(User)
+    end
+
+    it 'redirects to root path' do
+      expect(response).to redirect_to root_path
+    end
+  end
 end
--- spec/support/omniauth_macros.rb
+++ spec/support/omniauth_macros.rb
@@ -7,4 +7,13 @@ module OmniauthMacros
       credentials: { token: 'facebook_token' }
     )
   end
+
+  def mock_auth_twitter_hash
+    OmniAuth.config.mock_auth[:twitter] = OmniAuth::AuthHash.new(
+      provider: 'twitter',
+      uid: '12345',
+      info: { nickname: 'twitter_user', name: 'twitter_user_name', image: 'twitter_user_image_url' },
+      credentials: { token: 'twitter_token' }
+    )
+  end
 end
