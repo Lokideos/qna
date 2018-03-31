f4aacd1f26950b105f4dbe5eb223df4819845d31
lokideos <archmage09@gmail.com>
2018-03-31 22:58:12 +0300

    Add email confirmation and checking for email in auth hash

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :omniauthable, omniauth_providers: [:facebook, :twitter]
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable, :omniauthable, omniauth_providers: [:facebook, :twitter]
 
   has_many :answers, dependent: :destroy
   has_many :questions, dependent: :destroy
@@ -19,14 +19,22 @@ class User < ApplicationRecord
     return authorization.user if authorization
 
     email = auth.info[:email]
-    user = User.where(email: email).first
-    if user
-      user.create_authorization(auth)
+    if email
+      user = User.where(email: email).first
+      if user
+        user.create_authorization(auth)
+      else
+        password = Devise.friendly_token[0, 20]
+        user = User.create!(email: email, password: password, password_confirmation: password, confirmed_at: Time.now)
+        user.create_authorization(auth)
+      end
     else
-      password = Devise.friendly_token[0, 20]
-      user = User.create!(email: email, password: password, password_confirmation: password)
-      user.create_authorization(auth)
+      password = Devise.friendly_token[0,20]
+      email_part = Devise.friendly_token[0,8]
+      email = "#{email_part}@example.com"
+      user = User.new(email: email, password: password, password_confirmation: password)
     end
+
     user
   end
 
