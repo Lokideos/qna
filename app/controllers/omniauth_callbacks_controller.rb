95e3d9873adbba688c974b0e33c9c8a877883792
lokideos <archmage09@gmail.com>
2018-03-31 23:04:19 +0300

    authentication via twitter

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,6 +1,5 @@
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def facebook
-    # render json: request.env['omniauth.auth']
     @user = User.find_for_oauth(request.env['omniauth.auth'])
     if @user.persisted?
       sign_in_and_redirect @user, event: :authentication
@@ -9,6 +8,14 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
   end
 
   def twitter
-    render json: request.env['omniauth.auth']
+    @user = User.find_for_oauth(request.env['omniauth.auth'])
+    if @user.persisted?
+      sign_in_and_redirect @user, event: :authentication
+      set_flash_message(:notice, :success, kind: 'Twitter') if is_navigational_format?
+    elsif @user
+      session['device.provider'] = request.env['omniauth.auth'].provider
+      session['device.uid'] = request.env['omniauth.auth'].uid.to_s
+      redirect_to find_email_email_oauth_assigner_path
+    end
   end
 end
