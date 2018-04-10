d34c97a22281fcbe7078024c1dae97baab5e17fd
lokideos <archmage09@gmail.com>
2018-04-10 13:52:45 +0300

    Fix tests for cancelling ratings

--- spec/models/ability_spec.rb
+++ spec/models/ability_spec.rb
@@ -20,14 +20,18 @@ describe Ability do
   end
 
   describe 'for user' do
-    let(:user) { create :user }
+    let(:user) { create  :user }
     let(:other) { create :user }
-    let(:other2) { create :user }
+
     let(:question_rating) { create :rating, value: 1, ratable: create(:question, user: other), user: user }
-    let(:answer_rating) { create :rating, value: 1, ratable: create(:answer, user: other), user: user }
-    let(:answer_of_users_question) { create :answer, question: create(:question, user: user), user: user }
+    let(:answer_rating) { create :rating, value: 1, ratable: create(:answer, user: other),     user: user }
+    let(:bad_question_rating) { create :rating, value: 1, ratable: create(:question, user: user), user: other}
+    let(:bad_answer_rating) { create :rating, value: 1, ratable: create(:answer, user: user),     user: other}
+
+    let(:answer_of_users_question) { create :answer, question: create(:question, user: user),      user: user }
     let(:answer_of_not_users_question) { create :answer, question: create(:question, user: other), user: user }
-    let(:user_attachment) { create(:attachment, attachable: create(:question, user: user)) }
+
+    let(:user_attachment) { create(:attachment, attachable: create(:question,     user: user)) }
     let(:other_user_attachment) { create(:attachment, attachable: create(:answer, user: other)) }
 
     it { should_not be_able_to :manage, :all }
@@ -53,26 +57,26 @@ describe Ability do
     it { should_not be_able_to :destroy, create(:answer,   user: other), user: user }
     it { should_not be_able_to :destroy, create(:comment,  user: other), user: user }
 
-    it { should be_able_to :rate_good, question_rating.ratable, user: user }
-    it { should be_able_to :rate_good, answer_rating.ratable,   user: user }
-    it { should be_able_to :rate_bad, question_rating.ratable,  user: user }
-    it { should be_able_to :rate_bad, answer_rating.ratable,    user: user }
+    it { should be_able_to :rate_good, question_rating.ratable, user: other }
+    it { should be_able_to :rate_good, answer_rating.ratable,   user: other }
+    it { should be_able_to :rate_bad, question_rating.ratable,  user: other }
+    it { should be_able_to :rate_bad, answer_rating.ratable,    user: other }
 
     it { should_not be_able_to :rate_good, create(:question, user: user), user: user }
-    it { should_not be_able_to :rate_good, create(:answer, user: user),   user: user }
+    it { should_not be_able_to :rate_good, create(:answer,   user: user), user: user }
     it { should_not be_able_to :rate_bad, create(:question, user: user),  user: user }
-    it { should_not be_able_to :rate_bad, create(:answer, user: user),    user: user }
+    it { should_not be_able_to :rate_bad, create(:answer,   user: user),  user: user }
 
     it { should be_able_to :cancel_rate, question_rating.ratable, user: user }
     it { should be_able_to :cancel_rate, answer_rating.ratable,   user: user }
 
-    it { should_not be_able_to :cancel_rate, question_rating.ratable, user: other }
-    it { should_not be_able_to :cancel_rate, answer_rating.ratable,   user: other }
+    it { should_not be_able_to :cancel_rate, bad_question_rating.ratable, user: user }
+    it { should_not be_able_to :cancel_rate, bad_answer_rating.ratable,   user: user }
 
-    it { should be_able_to :choose_best, answer_of_users_question, user: user }
+    it { should be_able_to :choose_best, answer_of_users_question,         user: user }
     it { should_not be_able_to :choose_best, answer_of_not_users_question, user: user }
 
-    it { should be_able_to :destroy, user_attachment, user: user }
+    it { should be_able_to :destroy, user_attachment,           user: user }
     it { should_not be_able_to :destroy, other_user_attachment, user: user }
   end
 end
