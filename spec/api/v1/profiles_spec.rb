9fcecb6385b8d807e2e1efaf9177752b1db0e236
lokideos <archmage09@gmail.com>
2018-04-17 00:33:29 +0300

    User profiles API request functionality & tests

--- spec/api/v1/profiles_spec.rb
+++ spec/api/v1/profiles_spec.rb
@@ -37,4 +37,38 @@ RSpec.describe 'Profile API'  do
       end
     end
   end
+
+  describe 'GET /list' do
+    context 'unauthorized' do
+      it 'returns 401 status if there is no access_token' do
+        get '/api/v1/profiles/list', params: { format: :json }
+        expect(response.status).to eq 401
+      end
+
+      it 'returns 401 status if access_token is inalid' do
+        get '/api/v1/profiles/list', params: { format: :json, access_token: '1234' }
+        expect(response.status).to eq 401
+      end
+    end
+
+    context 'authorized' do
+      let(:me) { create(:user) }
+      let!(:users) { create_list(:user, 3) }
+      let(:access_token) { create(:access_token, resource_owner_id: me.id) }
+
+      before { get '/api/v1/profiles/list', params: { format: :json, access_token: access_token.token } }
+
+      it 'returns 200 status' do
+        expect(response).to be_success
+      end
+
+      it "contains users list" do        
+        expect(response.body).to be_json_eql(users.to_json)
+      end
+
+      it "does not contain current user" do
+        expect(response.body).to_not have_json_path(me.to_json)
+      end
+    end
+  end
 end
