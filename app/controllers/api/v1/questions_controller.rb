ff186a491f835d426fc1d555ac7b85c164a6a4ca
lokideos <archmage09@gmail.com>
2018-04-19 19:06:56 +0300

    Show question with comments and attachments but w/o answers API requests + tests

--- app/controllers/api/v1/questions_controller.rb
+++ app/controllers/api/v1/questions_controller.rb
@@ -1,6 +1,13 @@
 class Api::V1::QuestionsController < Api::V1::BaseController
+  authorize_resource
+
   def index
     @questions = Question.all
     respond_with @questions
+  end
+
+  def show
+    @question = Question.find(params[:id])
+    respond_with @question, serializer: QuestionWoAnswerSerializer
   end
 end
--- app/serializers/question_wo_answer_serializer.rb
+++ app/serializers/question_wo_answer_serializer.rb
@@ -1,6 +1,6 @@
-class QuestionSerializer < ActiveModel::Serializer
+class QuestionWoAnswerSerializer < ActiveModel::Serializer
   attributes :id, :title, :body, :created_at, :updated_at, :short_title
-  has_many :answers
+
   has_many :comments
   has_many :attachments
 
