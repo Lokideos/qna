69a965f15544eeaede87dfe746d7c0f11e9f4aee
lokideos <archmage09@gmail.com>
2018-04-19 19:12:54 +0300

    Add action for showing answers for API and refactoring

--- app/controllers/api/v1/questions_controller.rb
+++ app/controllers/api/v1/questions_controller.rb
@@ -1,4 +1,6 @@
 class Api::V1::QuestionsController < Api::V1::BaseController
+  before_action :load_question, only: [:show, :show_answers]
+  
   authorize_resource
 
   def index
@@ -7,7 +9,16 @@ class Api::V1::QuestionsController < Api::V1::BaseController
   end
 
   def show
-    @question = Question.find(params[:id])
     respond_with @question, serializer: QuestionWoAnswerSerializer
+  end
+
+  def show_answers
+    respond_with @question.answers
+  end
+
+  private
+
+  def load_question
+    @question = Question.find(params[:id])
   end
 end
