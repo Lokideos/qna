7f5f38f55dcfae0fed2d58b78abbc1088dc9133e
lokideos <archmage09@gmail.com>
2018-05-06 13:16:20 +0300

    Add authorization for profiles

--- app/controllers/api/v1/profiles_controller.rb
+++ app/controllers/api/v1/profiles_controller.rb
@@ -1,11 +1,11 @@
 class Api::V1::ProfilesController < Api::V1::BaseController
-  skip_authorization_check
-  
   def me
+    authorize! :read, current_resource_owner
     respond_with current_resource_owner
   end
 
   def index
+    authorize! :read, User.where.not(id: current_resource_owner.id)
     @profiles = User.all.where.not(id: current_resource_owner.id)
     respond_with @profiles
   end
